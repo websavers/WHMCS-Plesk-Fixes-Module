8773d06d8961883fefd61f871d7100041a08f2c7
Jordan Schelew <jas8522@gmail.com>
2016-09-06 15:03:34 -0300

    Restructure

---
+++ plesk_fixes/lang/english.php
@@ -0,0 +1,6 @@
+<?php
+
+$_ADDONLANG['intro'] = "This addon includes fixes for some common Plesk provisioning module issues";
+$_ADDONLANG['description'] = "WHMCS only generates 8 character passwords which aren't complex enough for the 'Very Strong' security function in Plesk 12+. This hook modifies the password prior to provisioning to make it more secure";
+$_ADDONLANG['documentation'] = "To use this module, configure it by supply an admin username in the module configuration settings under Setup > Addon Modules > Configure. That's all there is to it!";
+?>
