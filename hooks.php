aa362ba7224f1ab103e8f5089a7bf3453c4cf99a
jas8522 <jas8522@gmail.com>
2016-09-06 15:00:39 -0300

    Add initial files via upload

---
+++ hooks.php
@@ -0,0 +1,56 @@
+<?php
+
+if (!defined("WHMCS"))
+	die("This file cannot be accessed directly");
+
+
+/**
+ * Register Hooks
+ */
+
+/* Plesk Password complexity fix */
+add_hook("PreModuleCreate",1,"ws_plesk_pre_module_create");
+
+/**
+ * WHMCS only generates 8 character passwords which aren't complex enough
+ * for the "Very Strong" security function in Plesk 12+
+ * This hook modifies the password prior to provisioning to make it more secure
+ **/
+function ws_plesk_pre_module_create($vars){
+
+	if ($vars['params']['moduletype'] == 'plesk'){
+
+		$command = "updateclientproduct";
+		$adminuser = $vars['adminuser'];
+		$values["serviceid"] = $vars['params']['serviceid'];
+		$values["servicepassword"] = randomPassword(20);
+
+		$results = localAPI($command,$values,$adminuser);
+
+	}
+
+}
+
+/********************
+ * Helper Functions *
+ ********************/
+
+/**
+ * Pseudorandom password generator
+ * Limitations:
+ * - since rand() isn't truly random, it should be replaced with random_int()
+ *   if/when you switch to using PHP 7 (probably after WHMCS v7)
+ */
+
+function randomPassword($size = 8) {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-+=~';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < $size; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+
+?>
---
+++ plesk_fixes.php
@@ -0,0 +1,79 @@
+<?php
+
+if (!defined("WHMCS"))
+	die("This file cannot be accessed directly");
+
+/*
+**********************************************
+
+   *** Plesk Fixes ***
+
+This addon fixes some problems with WHMCS and Plesk provisioning
+
+Most important code for this addon is found in the hooks.php file.
+
+**********************************************
+*/
+
+function plesk_fixes_config() {
+    $configarray = array(
+	    "name" => "Plesk Fixes",
+	    "version" => "1.0",
+	    "author" => "Websavers Inc",
+	    "language" => "english",
+	    "fields" => array(
+				"adminuser" => array (
+						"FriendlyName" => "Admin User",
+						"Type" => "text",
+						"Size" => "25",
+						"Description" => "Supply an admin username that has full privileges",
+						"Default" => "admin",
+					),
+    	),
+		);
+    return $configarray;
+}
+
+function plesk_fixes_activate() {
+
+    # Return Result
+    return array('status'=>'success','description'=>'Congrats! The module is now activated.');
+    return array('status'=>'error','description'=>'Sorry, for some reason we couldn\'t activate this addon for WHMCS');
+    return array('status'=>'info','description'=>'Sup?');
+
+}
+
+function plesk_fixes_deactivate() {
+
+    # Return Result
+    return array('status'=>'success','description'=>'The addon has been successfully deactivated');
+    return array('status'=>'error','description'=>'Couldn\'t deactivate. WTF?');
+    return array('status'=>'info','description'=>'Sup?');
+
+}
+
+function plesk_fixes_upgrade($vars) {
+
+    $version = $vars['version'];
+
+}
+
+/***** Admin View *****/
+
+function plesk_fixes_output($vars) {
+
+    $modulelink = $vars['modulelink'];
+    $version = $vars['version'];
+    $LANG = $vars['_lang'];
+
+    echo '<p>'.$LANG['intro'].'</p>
+	<p>'.$LANG['description'].'</p>
+	<p>'.$LANG['documentation'].'</p>';
+
+}
+
+
+
+
+
+?>
