3ae044846d4329ccfee5988d1b75bf153dea2db6
Jordan Schelew <jordan@websavers.ca>
2019-08-07 12:43:01 -0300

    Works!
    - Use modern Capsule include use statement
    - Only run when password isn't long enough for Plesk (rule of thumb)
    - Randomly select symbols and capital letters to append to Plesk's not quite secure enough random passwords
    - Save new password to product AND send it to the module command via return values

--- plesk_fixes/hooks.php
+++ plesk_fixes/hooks.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Database\Capsule\Manager as Capsule;
+use WHMCS\Database\Capsule;
 
 if (!defined("WHMCS"))
 	die("This file cannot be accessed directly");
@@ -11,19 +11,46 @@ if (!defined("WHMCS"))
  * This hook modifies the password prior to provisioning to make it more secure
  **/
 
-add_hook("PreModuleCreate", 2, function($vars){
+add_hook("PreModuleCreate", 0, function($vars){
 	
-	if ( $vars['params']['moduletype'] == 'plesk' ){
+	//logActivity( "Service ID " . $vars['params']['serviceid'] . " with Password: " . $vars['params']['password'] ); ///DEBUG
+	
+	//14 is the default 'secure' password length that WHMCS creates
+	if ( $vars['params']['moduletype'] == 'plesk' && strlen($vars['params']['password']) <= 14 ){
 
+		$newpassword = $vars['params']['password'] . getchars(8); //append 8 chars to match Plesk policy
+		
 		//Change password saved in WHMCS for product
-		$values["serviceid"] = $vars['params']['serviceid'];
-		$password = decrypt($vars['params']['password'], $cc_encryption_hash) 
-		$values["servicepassword"] = $password . "!!!AZ"; //append chars to better match policy
-
-		$results = localAPI("updateclientproduct",$values);
-
+		$results = localAPI("updateclientproduct", array(
+			'serviceid' => $vars['params']['serviceid'],
+			'servicepassword' => $newpassword,
+		));
+		
+		if ($results['result'] != 'success') {
+		  logActivity( "[Plesk Fixes Module] An Error Occurred: " . $results['result'] );
+		}
+		else{
+			return array( //Send new data directly to module create function (so says the docs)
+				'password' => $newpassword,
+			);
+		}
+		
 	}
 
 });
+
+function getchars($str_len){
+	$str = '';
+	for($i = 0; $i < $str_len; $i++){
+		if ($i % 2 == 0){ //generate a letter when even, symbol when odd
+	    $str .= chr(rand(65, 90)); //65 = ascii for 'A' / 90 = ascii for 'Z'
+		}
+		else{
+			$symbols = '!@#$%^*?_~'; //Plesk supported symbols minus & due to WHMCS weirdness
+			$str .= $symbols[rand(0, strlen($symbols) - 1)]; 
+		}
+	}
+	return $str;
+}
 
 ?>
