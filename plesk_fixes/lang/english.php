509273b9d4c61df1ad5d0a0dddeacc51a62f10e1
Jordan Schelew <jordan@websavers.ca>
2022-12-28 22:53:54 -0600

    Update description

--- plesk_fixes/lang/english.php
+++ plesk_fixes/lang/english.php
@@ -2,5 +2,5 @@
 
 $_ADDONLANG['intro'] = "This addon includes fixes for some common Plesk provisioning module issues";
 $_ADDONLANG['description'] = "WHMCS only generates 8 character passwords which aren't complex enough for the 'Very Strong' security function in Plesk 12+. This hook modifies the password prior to provisioning to make it more secure";
-$_ADDONLANG['documentation'] = "To use this module, configure it by supply an admin username in the module configuration settings under Setup > Addon Modules > Configure. That's all there is to it!";
+$_ADDONLANG['documentation'] = "To use this module, simply activate it and it will do its magic.";
 ?>
